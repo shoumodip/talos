2a66378750ddbe9d073af3ebaf7a84dbdba38795
Shoumodip Kar <shoumodipkar@gmail.com>
2023-05-10 17:43:39 +0530

    Named color chooser

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const automata = [
     {
         "name": "Game Of Life",
-        "colors": ["#1e1e1e", "#ce9178"],
+        "colors": [
+            {"name": "Dead", "value": "#1e1e1e"},
+            {"name": "Alive", "value": "#ce9178"}
+        ],
         "update": (neighbours, current) => {
             switch (neighbours[1]) {
             case 3: return 1
@@ -13,7 +16,10 @@ const automata = [
 
     {
         "name": "Seeds",
-        "colors": ["#1e1e1e", "#ce9178"],
+        "colors": [
+            {"name": "Dead", "value": "#1e1e1e"},
+            {"name": "Alive", "value": "#ce9178"}
+        ],
         "update": (neighbours, current) => {
             return (neighbours[1] == 2) | 0
         }
@@ -21,7 +27,11 @@ const automata = [
 
     {
         "name": "Brian's Brain",
-        "colors": ["#1e1e1e", "#ce9178", "#9cdcfe"],
+        "colors": [
+            {"name": "Dead", "value": "#1e1e1e"},
+            {"name": "Alive", "value": "#ce9178"},
+            {"name": "Dying", "value": "#9cdcfe"}
+        ],
         "update": (neighbours, current) => {
             switch (current) {
             case 0: return (neighbours[1] == 2) | 0
@@ -33,7 +43,12 @@ const automata = [
 
     {
         "name": "Wireworld",
-        "colors": ["#1e1e1e", "#ce9178", "#9cdcfe", "#6a9955"],
+        "colors": [
+            {"name": "Empty", "value": "#1e1e1e"},
+            {"name": "Electron Head", "value": "#ce9178"},
+            {"name": "Electron Tail", "value": "#9cdcfe"},
+            {"name": "Conductor", "value": "#6a9955"}
+        ],
         "update": (neighbours, current) => {
             switch (current) {
             case 0: return 0
@@ -52,12 +67,12 @@ function render(ctx, board, colors) {
     const WIDTH = ctx.canvas.width / COLS
     const HEIGHT = ctx.canvas.height / ROWS
 
-    ctx.fillStyle = colors[0]
+    ctx.fillStyle = colors[0].value
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
     for (let row = 0; row < ROWS; row++) {
         for (let col = 0; col < COLS; col++) {
-            ctx.fillStyle = colors[board[row][col]]
+            ctx.fillStyle = colors[board[row][col]].value
             ctx.fillRect(col * WIDTH, row * HEIGHT, WIDTH, HEIGHT)
         }
     }
@@ -93,22 +108,30 @@ window.onload = () => {
     let automaton = automata[0]
 
     const colors = document.getElementById("colors")
-    colors.max = automaton.colors.length - 1
 
     colors.addEventListener("change", () => {
-        if (colors.validity.valid) {
-            color = colors.valueAsNumber
-        }
+        color = Number(colors.value)
     })
 
     const list = document.getElementById("list")
 
-    list.addEventListener("change", () => {
-        automaton = automata[Number(list.value)]
-        colors.max = automaton.colors.length - 1
+    function changeAutomaton(index) {
+        automaton = automata[index]
+        colors.replaceChildren(...automaton.colors.map((color, i) => {
+            const option = document.createElement("option")
+            option.text = color.name
+            option.value = i
+            return option
+        }))
         colors.value = 1
         color = 1
+    }
+
+    list.addEventListener("change", () => {
+        changeAutomaton(Number(list.value))
     })
+
+    changeAutomaton(0)
 
     automata.forEach((automaton, i) => {
         const option = document.createElement("option")
